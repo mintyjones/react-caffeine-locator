39e8c556f6f60677661e92290b0fd35ca05930b8
AnthonyCarroll97 <anthonycarroll1997@hotmail.com>
2021-06-11 12:04:43 +1000

    created attributions and loading screen components

---
+++ src/components/PlacesPanelLoading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PlacesPanelLoading() {
+    return (
+        <div className="h-full flex justify-center items-center animate-spin">
+            <img  width="40"className=""src="rotating-arrow.svg"></img>
+        </div>
+    )
+}
