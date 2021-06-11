57943844dd40f8553eab3a7a3cd68f5c056160e0
AnthonyCarroll97 <anthonycarroll1997@hotmail.com>
2021-06-11 11:48:09 +1000

    added more material ui icons, small styling changes

--- src/components/DetailsPanel.js
+++ src/components/DetailsPanel.js
@@ -2,13 +2,21 @@ import React from 'react'
 import { Rating } from '@material-ui/lab';
 import { Paper, Button } from '@material-ui/core'
 import Carousel from 'react-material-ui-carousel'
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 const DetailsPanel = ({ placeDetails, removeSelectedPlace }) => {
     console.log(placeDetails)
 
     const filterImages = () => {
+
         return placeDetails.photos.filter(photoObj => {
-            return photoObj.height < photoObj.width
+            if(photoObj.height < photoObj.width){
+                // pre-load the images
+                const picture = new Image
+                picture.src = photoObj.getUrl()
+                return true
+            } 
+            return false
         })
     }
     
@@ -16,7 +24,7 @@ const DetailsPanel = ({ placeDetails, removeSelectedPlace }) => {
         <div className="absolute top-1 right-1 z-10 bg-white p-2 w-5/12 max-w-lg rounded-lg">
             <div className="flex justify-between">
                 <h1 className="text-2xl">{placeDetails.name}</h1>
-                <button className="text-2xl px-2 bg-gray-200" onClick={removeSelectedPlace}>X</button>
+                <HighlightOffIcon style={{ fontSize: "40", cursor: "pointer" }}onClick={removeSelectedPlace}></HighlightOffIcon>
             </div>
             <Rating 
                 name="stars"
@@ -34,7 +42,7 @@ const DetailsPanel = ({ placeDetails, removeSelectedPlace }) => {
                 null
 
             }
-            <div className="max-w-full h-90 bg-red-200">
+            <div className="max-w-full">
                 <Carousel>
                     {filterImages().map((photo) => {
                         return <img src={photo.getUrl()}></img>
--- src/components/PlacesPanel.js
+++ src/components/PlacesPanel.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PlaceTile from './PlaceTile'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 const PlacesPanel = ({ places, getPlaceDetails }) => {
 
@@ -17,9 +19,39 @@ const PlacesPanel = ({ places, getPlaceDetails }) => {
     }
 
     return (
-        <div className="w-3/12 overflow-scroll h-screen">
-            { places.length > 1 ? renderPlaces(places) : <h1>Loading...</h1>}
+        <div className="w-3/12 h-screen">
+            <div className="overflow-y-auto h-5/6">
+                { places.length > 1 ? renderPlaces(places) : <h1>Loading...</h1>}
+            </div>
+            <div className="flex justify-evenly">
+                <div>
+                    <h3 className="text-lg">Gordon Campbell</h3>
+                    <div className=" flex justify-evenly">
+                        <a href="https://github.com/mintyjones">
+                            <GitHubIcon style={{ fontSize: "35"}}></GitHubIcon>
+                        </a>
+                        <a href="https://www.linkedin.com/in/gordon-campbell-3250439/">
+                            <LinkedInIcon style={{ fontSize: "45"}}></LinkedInIcon>
+                        </a>
+                    </div>
+                </div>
+                
+                
+                <div>
+                    <h3 className="text-lg">Anthony Carroll</h3>
+                    <div className="flex justify-evenly">
+                        <a href="https://github.com/AnthonyCarroll97">
+                            <GitHubIcon style={{ fontSize: "35"}}></GitHubIcon>
+                        </a>
+                        <a href="https://www.linkedin.com/in/anthony-carroll-3130661bb/">
+                            <LinkedInIcon style={{ fontSize: "45"}}></LinkedInIcon>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
+        
+        
     )
 }
 
